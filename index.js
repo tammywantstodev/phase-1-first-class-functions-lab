61c90c807b5a85fd88bc1f4f791974cf82faca60
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-11-26 14:35:56 +0300

    Save changes

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const returnFirstTwoDrivers=function(arr){
+    return arr.slice(0,2)
+  }
+  
+  const returnLastTwoDrivers=function(arr){
+    return arr.slice(arr.length-2)
+  }
+  let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+  
+  function createFareMultiplier(integer){
+    return function(fare){
+      return integer * fare;
+    }
+  }
+  const fareDoubler= createFareMultiplier(2);
+  
+  const fareTripler= createFareMultiplier(3);
+  
+  function selectDifferentDrivers(arr, callback){
+    return callback(arr);
+  }
+  
+  
+  
